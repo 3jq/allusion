4bf21873fbf91d1aa3497a369a3b028931c1a157
cattyngmd <cvenby@gmail.com>
2021-07-11 14:31:59 +0300

    Fix asap

--- src/main/java/dev/sl33py/allusion/api/setting/SettingManager.java
+++ src/main/java/dev/sl33py/allusion/api/setting/SettingManager.java
@@ -7,20 +7,22 @@ import java.util.*;
 
 public class SettingManager {
 
-    public List<Setting<?>> settingList;
+    public List<Setting> settingList;
 
-    public void register(Setting<?> s) { settingList.add(s); }
+    public SettingManager() {settingList = new ArrayList<>();}
 
-    public Setting<?> getSetting(String name) {
+    public void register(Setting s) { settingList.add(s); }
+
+    public Setting getSetting(String name) {
         return settingList.stream().filter(s -> s.getName().equalsIgnoreCase(name)).findFirst().orElse(null);
     }
 
-    public Setting<?> getSetting(String name, Module module) {
+    public Setting getSetting(String name, Module module) {
         return settingList.stream().filter(s -> s.getName().equalsIgnoreCase(name)).filter(s -> s.getModule().equals(module)).findFirst().orElse(null);
     }
 
-    public List<Setting<?>> getSettings(Module module) {
-        List<Setting<?>> list = new ArrayList<>();
+    public List<Setting> getSettings(Module module) {
+        List<Setting> list = new ArrayList<>();
         settingList.forEach(s -> {
             if(s.getModule().equals(module)) list.add(s);
         });
