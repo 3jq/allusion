ba494ea91c6ef79270d91ca4dcabef1eade9bab8
cattyngmd <cvenby@gmail.com>
2021-07-11 18:51:05 +0300

    Remove test thing

--- src/main/java/dev/sl33py/allusion/client/module/modules/client/ClickGuiModule.java
+++ src/main/java/dev/sl33py/allusion/client/module/modules/client/ClickGuiModule.java
@@ -14,7 +14,6 @@ public class ClickGuiModule extends Module {
     public Setting<Double> g = register("Green", 0, 0, 255, 0);
     public Setting<Double> b = register("Blue", 255, 0, 255, 0);
     public Setting<Boolean> tint = register("Tint", true);
-    Setting<String> kek = register("Mode","1", Arrays.asList("1", "2", "3", "asdasd4"));
 
     public ClickGuiModule() {
         super("ClickGui", "Shows client ClickGui", Category.Client);
